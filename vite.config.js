543e7bfabfd83b12935f283372ee1e57292feb39
JVKalmekar <jvkalmekardcad@gmail.com>
2026-01-29 18:11:55 +0530

    Removed reactRouter from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import babel from 'vite-plugin-babel';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { reactRouter } from "@react-router/dev/vite";
 
-const ReactCompilerConfig = { /* ... */ };
+
+
 
 
 // https://vite.dev/config/
 
 export default defineConfig({
     plugins: [
-    reactRouter(),
     react({
       babel : {
-      plugins: ["babel-plugin-react-compiler", ReactCompilerConfig],
+      plugins: ["babel-plugin-react-compiler"],
         
      },
   }),
